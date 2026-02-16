00a459bd46e820330fa134dbeec18e0389ee96ba
nejaskiewicz-eng <n.e.jaskiewicz@gmail.com>
2026-02-16 02:23:03 +0100

    Update participants.js

--- api/admin-api/participants.js
+++ api/admin-api/participants.js
@@ -1,6 +1,2 @@
 // api/admin-api/participants.js
-module.exports = async (req, res) => {
-  req.query = req.query || {};
-  req.query.path = ["participants"];
-  return require("./[...path].js")(req, res);
-};
+module.exports = require("./participants/index.js");
