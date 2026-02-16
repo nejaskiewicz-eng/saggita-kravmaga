94935c5735c87f08ba81da826938ec34411298ff
nejaskiewicz-eng <n.e.jaskiewicz@gmail.com>
2026-02-16 10:34:09 +0100

    Create db.js

---
+++ api/_lib/db.js
@@ -0,0 +1,16 @@
+// api/_lib/db.js
+import pg from "pg";
+
+const { Pool } = pg;
+
+let pool;
+
+export function getPool() {
+  if (!pool) {
+    pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    });
+  }
+  return pool;
+}
