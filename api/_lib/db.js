1428b0bdb90a6f398f9dd44cb6bc5630ded55601
nejaskiewicz-eng <n.e.jaskiewicz@gmail.com>
2026-02-16 15:10:58 +0100

    Update db.js

--- api/_lib/db.js
+++ api/_lib/db.js
@@ -1,16 +1,26 @@
-// api/_lib/db.js
+// /api/_lib/db.js
 import pg from "pg";
 
 const { Pool } = pg;
 
-let pool;
+let _pool;
 
 export function getPool() {
-  if (!pool) {
-    pool = new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
-    });
+  if (_pool) return _pool;
+
+  const conn =
+    process.env.DATABASE_URL ||
+    process.env.POSTGRES_URL ||
+    process.env.NEON_DATABASE_URL;
+
+  if (!conn) {
+    throw new Error("Brak DATABASE_URL w zmiennych środowiskowych na Vercel.");
   }
-  return pool;
+
+  _pool = new Pool({
+    connectionString: conn,
+    ssl: { rejectUnauthorized: false },
+  });
+
+  return _pool;
 }
